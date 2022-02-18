d0db627bc6c42e769fdeec555defcc0eed24b581
vanesa_picazo <vanesapicazoagusti@gmail.com>
2022-02-18 16:51:20 +0100

    Ejercicios del 2 al 5

---
+++ Ejercicio 4/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    Rigidbody2D myRB;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRB = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float move = Input.GetAxis("Horizontal");
+        myRB.velocity = new Vector2(move * speed, myRB.velocity.y);
+    }
+}
---
+++ Ejercicio 5/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    Rigidbody2D myRB;
+    public float speed;
+    GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRB = GetComponent<Rigidbody2D>();
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float move = Input.GetAxis("Horizontal");
+        myRB.velocity = new Vector2(move * speed, myRB.velocity.y);
+
+        if (move < 0)
+        {
+            player.GetComponent<SpriteRenderer>().flipX = true;
+        }
+        if(move > 0)
+        {
+            player.GetComponent <SpriteRenderer>().flipX = false;
+        }
+    }
+}
